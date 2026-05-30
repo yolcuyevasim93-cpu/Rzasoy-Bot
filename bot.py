685ef703988c252fff15e636d015cae66a5ebd19
yolcuyevasim93-cpu <yolcuyevasim93@gmail.com>
2026-05-31 03:00:04 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,12 @@
 import os
-import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
 from flask import Flask
 from threading import Thread
 
-# Bot sazlamaları
-BOT_TOKEN = "8421111075:AAGCv5a7M1YyrCrHsrS11780ca17dNDHHKI"
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+# Botun sazlamaları
+TOKEN = "8421111075:AAGCv5a7M1YyrCrHsrS11780ca17dNDHHKI"
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
-# Flask sazlaması (Render-in servisi dayandırmaması üçün)
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,14 +17,8 @@ def run_flask():
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
-# Botun işə düşməsi
-async def main():
-    print("Bot işə düşdü...")
-    await dp.start_polling(bot)
-
+# Botu işə salmaq üçün (sadəcə polling)
 if __name__ == "__main__":
-    # Flask-ı arxa planda işə salırıq
     Thread(target=run_flask).start()
-    # Botu əsas dövrədə işə salırıq
-    asyncio.run(main())
-  
+    dp.run_polling(bot)
+    
